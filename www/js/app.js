3d910c98a119d539be3ad944a1232149959e0f1d
aurex77 <jouanisson.aurelien@gmail.com>
2017-03-15 11:25:52 +0100

    css pages home + calendrier

--- www/js/app.js
+++ www/js/app.js
@@ -8,68 +8,79 @@ const PATH_MAC = "https://connectmangas.com/server/";
 
 angular.module('starter', ['ionic', 'starter.controllers'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+    .run(function($ionicPlatform) {
+        $ionicPlatform.ready(function() {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-
-    .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
-  })
-
-  .state('app.search', {
-    url: '/search',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/search.html'
-      }
-    }
-  })
-
-  .state('app.homepage', {
-      url: '/homepage',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/homepage.html'
-        }
-      }
-    })
-    .state('app.calendrier', {
-      url: '/calendrier',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/calendrier.html',
-          controller: 'CalendrierCtrl'
-        }
-      }
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
+        });
     })
 
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
-      }
-    }
-  });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/homepage');
-});
+    .config(function($stateProvider, $urlRouterProvider) {
+        $stateProvider
+
+            .state('app', {
+                url: '/app',
+                abstract: true,
+                templateUrl: 'templates/menu.html',
+                controller: 'AppCtrl'
+            })
+
+            .state('app.search', {
+                url: '/search',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/search.html'
+                    }
+                }
+            })
+
+            .state('app.homepage', {
+                url: '/homepage',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/homepage.html'
+                    }
+                }
+            })
+
+            .state('app.calendrier', {
+                url: '/calendrier',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/calendrier.html',
+                        controller: 'CalendrierCtrl'
+                    }
+                }
+            })
+
+            .state('app.single', {
+                url: '/playlists/:playlistId',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/playlist.html',
+                        controller: 'PlaylistCtrl'
+                    }
+                }
+            })
+
+            .state('app.anime', {
+                url: '/anime/:animeID',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/anime.html',
+                        controller: 'AnimeController'
+                    }
+                }
+            });
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/app/homepage');
+    });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,43 +2,43 @@ angular.module('starter.controllers', [])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+    // With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+    // listen for the $ionicView.enter event:
+    //$scope.$on('$ionicView.enter', function(e) {
+    //});
 
-  // Form data for the login modal
-  $scope.loginData = {};
+    // Form data for the login modal
+    $scope.loginData = {};
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
+    // Create the login modal that we will use later
+    $ionicModal.fromTemplateUrl('templates/login.html', {
+        scope: $scope
+    }).then(function(modal) {
+        $scope.modal = modal;
+    });
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
+    // Triggered in the login modal to close it
+    $scope.closeLogin = function() {
+        $scope.modal.hide();
+    };
 
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
+    // Open the login modal
+    $scope.login = function() {
+        $scope.modal.show();
+    };
 
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
+    // Perform the login action when the user submits the login form
+    $scope.doLogin = function() {
+        console.log('Doing login', $scope.loginData);
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
+        // Simulate a login delay. Remove this and replace with your login
+        // code if using a login system
+        $timeout(function() {
+            $scope.closeLogin();
+        }, 1000);
+    };
 })
 
 .factory('calendarService', function($http) {
@@ -65,20 +65,20 @@ angular.module('starter.controllers', [])
 
 .controller('CalendrierCtrl', function($scope, calendarService) {
 
-  $scope.calendar = "animes";
+    $scope.calendar = "animes";
 
-  $scope.displayCalendar = function(){
-      var calendar = calendarService.getCalendar($scope.calendar);
+    $scope.displayCalendar = function(value){
+        var calendar = calendarService.getCalendar(value);
 
-      calendar.then(function(response) {
-          if ( response.status == 200 ) {
-              $scope.days = response.infos;
-          }
+        calendar.then(function(response) {
+            if ( response.status == 200 ) {
+                $scope.days = response.infos;
+            }
 
-      });
-  }
+        });
+    }
 
-  $scope.displayCalendar();
+    $scope.displayCalendar();
 })
 
 .controller('PlaylistCtrl', function($scope, $stateParams) {
