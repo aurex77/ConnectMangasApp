a014a6844c74ef4a692f700dfdc98af6783684b2
Medardy <medard.youssef@hotmail.fr>
2017-05-19 16:53:25 +0200

    Add dossier connectMobile

---
+++ ConnectMobile/src/pages/Calendrier/calendrier.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { Calendrier } from './calendrier';
+
+@NgModule({
+  declarations: [
+    Calendrier,
+  ],
+  imports: [
+    IonicPageModule.forChild(Calendrier),
+  ],
+  exports: [
+    Calendrier
+  ]
+})
+export class CalendrierdModule {}
